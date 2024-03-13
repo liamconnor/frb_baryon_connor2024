b50a45f1e35bf605449148f9a77201597f53ea74
liamconnor <liam.dean.connor@gmail.com>
2024-03-13 09:11:19 -0700

    adding a new reader feature to exclude sources by name

--- src/reader.py
+++ src/reader.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def read_frb_catalog(fnfrb, zmin=0.0, zmax=np.inf, 
                      telescope='all', secure_host=True,
-                     max_fractional_MWDM=np.inf):
+                     max_fractional_MWDM=np.inf, exclude_names=None):
     # Read the FRB catalog from a csv file
     frb_catalog = pd.read_csv(fnfrb, delim_whitespace=False)
     zfrb = frb_catalog['redshift'].values
@@ -30,5 +30,12 @@ def read_frb_catalog(fnfrb, zmin=0.0, zmax=np.inf,
         frac_mw = frb_catalog['ne2001'].values / frb_catalog['dm_opt'].values
         ind = np.where(frac_mw < max_fractional_MWDM)[0]
         frb_catalog = frb_catalog.iloc[ind]
+
+    if exclude_names is not None:
+        if type(exclude_names) != list:
+            exclude_names = [exclude_names]
+        for name in exclude_names:
+            indexNames = frb_catalog[ frb_catalog['name'] == name].index
+            frb_catalog.drop(indexNames, inplace=True)
 
     return frb_catalog
